b75d0de1dd2a7d308b8e0fe982f84c80e9077dfe
CX33071 <3541053286@qq.com>
2026-06-04 15:28:40 +0800

    note

---
+++ FTP客户端.cc
@@ -0,0 +1,358 @@
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <cerrno>
+#include <cstring>
+#include <iostream>
+#include <string>
+
+class Socket {
+   public:
+    Socket(int domain, int type, int protocol);
+    ~Socket();
+    int fd();
+    bool isvalid();
+    bool enableport();
+
+   private:
+    int fd_ = -1;
+};
+
+class FTPClient {
+   public:
+    FTPClient(uint16_t port, std::string ip);
+    void start();
+
+   private:
+    bool login();
+    void LIST(std::string args);
+    void RETR(std::string args);
+    void STOR(std::string args);
+    int PASV();
+    void sendmessage(std::string cmd);
+    int getcode(std::string line);
+    bool prasePASV(std::string resp, std::string& ip, int& port);
+    std::string getfilename(std::string& path);
+    void getcmd(std::string& input, std::string& first, std::string& second);
+
+    Socket Client_;
+};
+
+std::string getlineok(std::string line);
+std::string readline(int fd);
+bool sendn(int fd, char* s, size_t len);
+
+Socket::Socket(int domain, int type, int protocol)
+    : fd_(socket(domain, type, protocol)) {}
+
+Socket::~Socket() {
+    if (fd_ != -1) {
+        close(fd_);
+        fd_ = -1;
+    }
+}
+
+int Socket::fd() {
+    return fd_;
+}
+
+bool Socket::isvalid() {
+    return fd_ != -1;
+}
+
+bool Socket::enableport() {
+    int opt = 1;
+    if (setsockopt(fd_, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
+        std::cout << strerror(errno) << std::endl;
+        return false;
+    }
+    return true;
+}
+
+FTPClient::FTPClient(uint16_t port, std::string ip)
+    : Client_(AF_INET, SOCK_STREAM, 0) {
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    inet_pton(AF_INET, ip.c_str(), &addr.sin_addr);
+    connect(Client_.fd(), reinterpret_cast<sockaddr*>(&addr), sizeof(addr));
+}
+
+void FTPClient::start() {
+    login();
+    std::cout << "可用命令:PASV | LIST | RETR | STOR | QUIT\n";
+    std::string line;
+    while (1) {
+        std::cout << "ftp: " << std::endl;
+        std::getline(std::cin, line);
+        std::string cmd;
+        std::string rest;
+        size_t pos = line.find(' ');
+        if (pos == std::string::npos) {
+            cmd = line;
+        } else {
+            cmd = line.substr(0, pos);
+            rest = line.substr(pos + 1);
+        }
+        if (!strcasecmp(cmd.c_str(), "PASV")) {
+            int datafd = PASV();
+            if (datafd != -1) {
+                close(datafd);
+            }
+        } else if (!strcasecmp(cmd.c_str(), "LIST")) {
+            LIST(rest);
+        } else if (!strcasecmp(cmd.c_str(), "RETR")) {
+            RETR(rest);
+        } else if (!strcasecmp(cmd.c_str(), "STOR")) {
+            STOR(rest);
+        } else if (!strcasecmp(cmd.c_str(), "QUIT")) {
+            sendmessage("QUIT");
+            std::cout << "成功退出";
+            break;
+        } else {
+            std::cout << "不支持的命令\n";
+        }
+    }
+}
+
+bool FTPClient::login() {
+    std::string user;
+    std::string pass;
+
+    std::cout << "请输入用户名: " << std::endl;
+    std::getline(std::cin, user);
+    sendmessage("USER " + user);
+    std::string resp = readline(Client_.fd());
+    if (getcode(resp) == 530) {
+        std::cout << "该用户不存在" << std::endl;
+    }
+    std::cout << "请输入密码: " << std::endl;
+    if (!std::getline(std::cin, pass)) {
+        return false;
+    }
+    sendmessage("PASS " + pass);
+    resp = readline(Client_.fd());
+    if (getcode(resp) == 530) {
+        std::cout << "密码错误" << std::endl;
+    } else {
+        std::cout << "成功登录" << std::endl;
+    }
+    return true;
+}
+
+void FTPClient::LIST(std::string args) {
+    int datafd = PASV();
+    sendmessage("LIST " + args);
+    std::string resp = readline(Client_.fd());
+    if (getcode(resp) == 550) {
+        std::cout << "该目录不存在" << std::endl;
+        return;
+    }
+    char buf[100];
+    ssize_t n = 0;
+    while ((n = recv(datafd, buf, sizeof(buf), 0)) > 0) {
+        write(1, buf, n);
+    }
+    close(datafd);
+
+    resp = readline(Client_.fd());
+
+    if (getcode(resp) == 226) {
+        std::cout << "目录已全部展示成功" << std::endl;
+    }
+}
+
+void FTPClient::RETR(std::string args) {
+    std::string remote;
+    std::string local;
+    getcmd(args, remote, local);
+    if (remote.empty()) {
+        std::cout << "用法: RETR 远程文件 本地文件\n";
+        return;
+    }
+    if (local.empty()) {
+        local = getfilename(remote);
+    }
+    int fd = open(local.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd == -1) {
+        std::cout << "本地文件创建失败！" << std::endl;
+        return;
+    }
+    int datafd = PASV();
+    sendmessage("RETR " + remote + " " + local);
+    std::string resp = readline(Client_.fd());
+    if (getcode(resp) == 550) {
+        std::cout << "远程文件不存在!" << std::endl;
+        return;
+    }
+    char buf[100];
+    ssize_t n = 0;
+    while ((n = recv(datafd, buf, sizeof(buf), 0)) > 0) {
+        write(fd, buf, n);
+    }
+    close(fd);
+    close(datafd);
+    resp = readline(Client_.fd());
+
+    if (getcode(resp) == 226) {
+        std::cout << "下载成功\n";
+    } else {
+        std::cout << "连接关闭，下载失败" << std::endl;
+    }
+}
+void FTPClient::STOR(std::string args) {
+    std::string local;
+    std::string remote;
+    getcmd(args, local, remote);
+    if (local.empty()) {
+        std::cout << "用法: STOR 本地文件 远程文件\n";
+        return;
+    }
+    if (remote.empty()) {
+        remote = getfilename(local);
+    }
+    int fd = open(local.c_str(), O_RDONLY);
+    if (fd == -1) {
+        std::perror("open");
+        return;
+    }
+    int datafd = PASV();
+    sendmessage("STOR " + local + " " + remote);
+    std::string resp = readline(Client_.fd());
+    if (getcode(resp) == 550) {
+        std::cout << "远程文件创建失败!" << std::endl;
+        return;
+    }
+    char buf[4096];
+    ssize_t n = 0;
+    while ((n = read(fd, buf, sizeof(buf))) > 0) {
+        if (!sendn(datafd, buf, static_cast<size_t>(n))) {
+            perror("sendn");
+            return;
+        };
+    }
+    close(fd);
+    close(datafd);
+    resp = readline(Client_.fd());
+    if (getcode(resp) == 226) {
+        std::cout << "上传成功\n";
+    } else {
+        std::cout << "上传失败，连接断开" << std::endl;
+    }
+}
+
+int FTPClient::PASV() {
+    sendmessage("PASV");
+    std::string resp = readline(Client_.fd());
+    std::string ip;
+    int port = 0;
+    prasePASV(resp, ip, port);
+    int datafd = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(static_cast<uint16_t>(port));
+    inet_pton(AF_INET, ip.c_str(), &addr.sin_addr);
+    connect(datafd, reinterpret_cast<sockaddr*>(&addr), sizeof(addr));
+    std::cout << "数据通道已经连接成功\n";
+    return datafd;
+}
+
+void FTPClient::sendmessage(std::string cmd) {
+    std::string line = cmd + "\r\n";
+    if (!sendn(Client_.fd(), line.data(), line.size())) {
+        perror("sendn");
+        return;
+    };
+}
+
+int FTPClient::getcode(std::string line) {
+    if (line.size() < 3) {
+        return -1;
+    }
+    return std::atoi(line.substr(0, 3).c_str());
+}
+
+bool FTPClient::prasePASV(std::string resp, std::string& ip, int& port) {
+    size_t left = resp.find('(');
+    size_t right = resp.find(')');
+    if (left == std::string::npos || right == std::string::npos ||
+        right <= left + 1) {
+        return false;
+    }
+
+    int h1 = 0, h2 = 0, h3 = 0, h4 = 0, p1 = 0, p2 = 0;
+    if (std::sscanf(resp.substr(left + 1, right - left - 1).c_str(),
+                    "%d,%d,%d,%d,%d,%d", &h1, &h2, &h3, &h4, &p1, &p2) != 6) {
+        return false;
+    }
+
+    ip = std::to_string(h1) + "." + std::to_string(h2) + "." +
+         std::to_string(h3) + "." + std::to_string(h4);
+    port = p1 * 256 + p2;
+    return true;
+}
+
+std::string FTPClient::getfilename(std::string& path) {
+    size_t pos = path.find_last_of("/");
+    return pos == std::string::npos ? path : path.substr(pos + 1);
+}
+
+void FTPClient::getcmd(std::string& input,
+                       std::string& first,
+                       std::string& second) {
+    size_t pos = input.find(' ');
+    if (pos == std::string::npos) {
+        first = getlineok(input);
+        return;
+    }
+    first = getlineok(input.substr(0, pos));
+    second = getlineok(input.substr(pos + 1));
+}
+
+std::string getlineok(std::string line) {
+    while (!line.empty() && (line.back() == '\n' || line.back() == '\r')) {
+        line.pop_back();
+    }
+    return line;
+}
+
+std::string readline(int fd) {
+    std::string line;
+    char ch = 0;
+    while (true) {
+        ssize_t n = recv(fd, &ch, 1, 0);
+        if (n <= 0) {
+            return "";
+        }
+        line.push_back(ch);
+        if (ch == '\n') {
+            break;
+        }
+    }
+    return getlineok(line);
+}
+
+bool sendn(int fd, char* data, size_t len) {
+    while (len > 0) {
+        ssize_t n = send(fd, data, len, 0);
+        if (n <= 0) {
+            return false;
+        }
+        data += n;
+        len -= static_cast<size_t>(n);
+    }
+    return true;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "serverip\n";
+        return 1;
+    }
+
+    FTPClient client(2100, argv[1]);
+    client.start();
+    return 0;
+}
---
+++ epoll服务端.cc
@@ -0,0 +1,536 @@
+#include <arpa/inet.h>
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <condition_variable>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <mutex>
+#include <queue>
+#include <string>
+#include <thread>
+#include <vector>
+#define MAX_SIZE 100
+
+class Socket {
+   public:
+    Socket(int domain, int type, int protocol);
+    ~Socket();
+    int fd();
+    bool isvalid();
+    bool enableport();
+
+   private:
+    int fd_ = -1;
+};
+
+class IPV4 {
+   public:
+    IPV4(uint16_t port);
+    sockaddr* change();
+    socklen_t len();
+    uint16_t getport();
+
+   private:
+    sockaddr_in addr_{};
+};
+
+struct Client {
+    bool hasuser = false;
+    bool islogin = false;
+    std::string username;
+    std::mutex mutex;
+    int pasvfd = -1;
+    std::string readbuf;
+    std::string writebuf;
+};
+class Threadpool {
+   public:
+    Threadpool(ssize_t num = std::thread::hardware_concurrency());
+    ~Threadpool();
+    void addtask(std::function<void()> task);
+
+   private:
+    void work();
+
+    bool stop_ = false;
+    std::mutex mutex_;
+    std::condition_variable cond_;
+    std::queue<std::function<void()>> tasks_;
+    std::vector<std::thread> threads_;
+};
+class ftpepollserver {
+   public:
+    ftpepollserver(uint16_t port);
+    ~ftpepollserver();
+    void run();
+
+   private:
+    void handle_read(int fd);
+    void handle_write(int fd);
+    void add_epoll(int fd, uint32_t events);
+    void del_epoll(int fd, uint32_t events);
+    void mod_epoll(int fd, uint32_t events);
+    void USER(int cfd, Client& client, std::string& user);
+    void PASS(int cfd, Client& client, std::string& pass);
+    void PASV(int cfd, Client& client);
+    int acceptdata(Client& client);
+    void LIST(int cfd, Client& client, std::string args);
+    void RETR(int cfd, Client& client, std::string path1, std::string path2);
+    void STOR(int cfd, Client& client, std::string path1, std::string path2);
+    void closePASV(Client& client);
+
+    uint16_t port_;
+    Socket listensock_;
+    int epfd;
+    Threadpool pool;
+    std::map<int, Client> clients;
+    std::map<std::string, std::string> account;
+};
+
+std::string getlineok(std::string line);
+bool sendn(int fd, char* data, ssize_t len);
+void set_nonblock(int fd);
+Socket::Socket(int domain, int type, int protocol)
+    : fd_(socket(domain, type, protocol)) {}
+
+Socket::~Socket() {
+    if (fd_ != -1) {
+        close(fd_);
+        fd_ = -1;
+    }
+}
+
+int Socket::fd() {
+    return fd_;
+}
+
+bool Socket::isvalid() {
+    return fd_ != -1;
+}
+
+bool Socket::enableport() {
+    int opt = 1;
+    if (setsockopt(fd_, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
+        std::cout << strerror(errno) << std::endl;
+        return false;
+    }
+    return true;
+}
+
+IPV4::IPV4(uint16_t port) {
+    addr_.sin_family = AF_INET;
+    addr_.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr_.sin_port = htons(port);
+}
+
+sockaddr* IPV4::change() {
+    return reinterpret_cast<sockaddr*>(&addr_);
+}
+
+socklen_t IPV4::len() {
+    return sizeof(addr_);
+}
+
+uint16_t IPV4::getport() {
+    return ntohs(addr_.sin_port);
+}
+
+Threadpool::Threadpool(ssize_t num) {
+    if (num == 0) {
+        num = 4;
+    }
+    for (ssize_t i = 0; i < num; ++i) {
+        threads_.emplace_back(&Threadpool::work, this);
+    }
+}
+
+Threadpool::~Threadpool() {
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        stop_ = true;
+    }
+    cond_.notify_all();
+    for (auto& t : threads_) {
+        if (t.joinable()) {
+            t.join();
+        }
+    }
+}
+
+void Threadpool::addtask(std::function<void()> task) {
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        tasks_.push(std::move(task));
+    }
+    cond_.notify_one();
+}
+
+void Threadpool::work() {
+    while (true) {
+        std::function<void()> task;
+        {
+            std::unique_lock<std::mutex> lock(mutex_);
+            cond_.wait(lock, [this]() { return stop_ || !tasks_.empty(); });
+            if (stop_ && tasks_.empty()) {
+                return;
+            }
+            task = std::move(tasks_.front());
+            tasks_.pop();
+        }
+        task();
+    }
+}
+ftpepollserver::ftpepollserver(uint16_t port)
+    : port_(port), listensock_(AF_INET, SOCK_STREAM, 0) {
+    epfd = epoll_create1(0);
+    account["ftp"] = "123456";
+}
+ftpepollserver::~ftpepollserver() {
+    close(epfd);
+}
+void ftpepollserver::run() {
+    listensock_.enableport();
+    IPV4 addr(port_);
+    bind(listensock_.fd(), addr.change(), addr.len());
+    listen(listensock_.fd(), 10);
+    set_nonblock(listensock_.fd());
+    std::cout << "ftp server listen" << port_ << '\n';
+    add_epoll(listensock_.fd(), EPOLLIN | EPOLLET);
+    struct epoll_event events[MAX_SIZE];
+    while (1) {
+        int n = epoll_wait(epfd, events, MAX_SIZE, -1);
+        for (int i = 0; i < n; i++) {
+            int fd = events[i].data.fd;
+            if (fd == listensock_.fd()) {
+                while (1) {
+                    int cfd = accept(listensock_.fd(), nullptr, nullptr);
+                    if (cfd == -1) {
+                        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                            break;
+                        } else {
+                            perror("accept");
+                            break;
+                        }
+                    }
+                    std::cout << "有一个客户端成功连接\n";
+                    set_nonblock(cfd);
+                    add_epoll(cfd, EPOLLIN | EPOLLET);
+                }
+            } else if (events[i].events & EPOLLIN) {
+                handle_read(fd);
+            } else if (events[i].events & EPOLLOUT) {
+                handle_write(fd);
+            }
+        }
+    }
+}
+void ftpepollserver::handle_read(int fd) {
+    ssize_t n;
+    char buf[4096];
+    while (1) {
+        n = read(fd, buf, sizeof(buf));
+        if (n > 0) {
+            clients[fd].readbuf.append(buf, n);
+        } else if (n == 0) {
+            close(fd);
+            clients.erase(fd);
+            del_epoll(fd, 0);
+            return;
+        } else {
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                break;
+            } else {
+                close(fd);
+                return;
+            }
+        }
+    }
+    while (1) {
+        ssize_t pos = clients[fd].readbuf.find("\n");
+        if (pos == std::string::npos) {
+            break;
+        }
+        std::string s = clients[fd].readbuf.substr(0, pos + 1);
+        clients[fd].readbuf.erase(0, pos + 1);
+        s = getlineok(s);
+        std::string cmd, arg;
+        int p = s.find(' ');
+        if (p == std::string::npos) {
+            cmd = s;
+        } else {
+            cmd = s.substr(0, p);
+            arg = s.substr(p + 1);
+        }
+        if (cmd == "USER") {
+            USER(fd, clients[fd], arg);
+        } else if (cmd == "PASS") {
+            PASS(fd, clients[fd], arg);
+        } else if (cmd == "QUIT") {
+            std::cout << "有一个客户端成功退出" << std::endl;
+        } else if (!strcasecmp(cmd.c_str(), "pasv")) {
+            PASV(fd, clients[fd]);
+        } else if (!strcasecmp(cmd.c_str(), "list")) {
+            pool.addtask([this, fd, arg]() { LIST(fd, clients[fd], arg); });
+        } else if (!strcasecmp(cmd.c_str(), "stor")) {
+            int pos = arg.find(' ');
+            std::string s1 = arg.substr(0, pos);
+            std::string s2 = arg.substr(pos + 1);
+
+            pool.addtask(
+                [this, fd, s1, s2]() { STOR(fd, clients[fd], s1, s2); });
+        } else if (!strcasecmp(cmd.c_str(), "retr")) {
+            int pos = arg.find(' ');
+            std::string s1 = arg.substr(0, pos);
+            std::string s2 = arg.substr(pos + 1);
+            pool.addtask(
+                [this, fd, s1, s2]() { RETR(fd, clients[fd], s1, s2); });
+        }
+    }
+}
+void ftpepollserver::handle_write(int fd) {
+    ssize_t n;
+    std::lock_guard<std::mutex> lock(clients[fd].mutex);
+    while (!clients[fd].writebuf.empty()) {
+        n = send(fd, clients[fd].writebuf.data(), clients[fd].writebuf.size(),
+                 0);
+        if (n > 0) {
+            clients[fd].writebuf.erase(0, n);
+        } else if (n < 0) {
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                break;
+            }
+            close(fd);
+            clients.erase(fd);
+            del_epoll(fd, 0);
+            return;
+        }
+    }
+    if (clients[fd].writebuf.empty()) {
+        mod_epoll(fd, EPOLLET | EPOLLIN);
+    } else {
+        mod_epoll(fd, EPOLLIN | EPOLLET | EPOLLOUT);
+    }
+}
+void ftpepollserver::add_epoll(int fd, uint32_t events) {
+    epoll_event event{};
+    event.events = events;
+    event.data.fd = fd;
+    epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &event);
+}
+
+void ftpepollserver::del_epoll(int fd, uint32_t events) {
+    epoll_event event{};
+    event.events = events;
+    event.data.fd = fd;
+    epoll_ctl(epfd, EPOLL_CTL_DEL, fd, &event);
+}
+void ftpepollserver::mod_epoll(int fd, uint32_t events) {
+    epoll_event event{};
+    event.events = events;
+    event.data.fd = fd;
+    epoll_ctl(epfd, EPOLL_CTL_MOD, fd, &event);
+}
+void ftpepollserver::USER(int cfd, Client& client, std::string& user) {
+    if (account.count(user) == 0) {
+        std::lock_guard<std::mutex> lock(clients[cfd].mutex);
+        clients[cfd].writebuf += "530 username disexist\r\n";
+        mod_epoll(cfd, EPOLLIN | EPOLLOUT | EPOLLET);
+        return;
+    } else {
+        std::lock_guard<std::mutex> lock(clients[cfd].mutex);
+        clients[cfd].writebuf += "222 username exist\r\n";
+        mod_epoll(cfd, EPOLLIN | EPOLLOUT | EPOLLET);
+    }
+    client.username = user;
+    client.hasuser = true;
+    client.islogin = false;
+}
+
+void ftpepollserver::PASS(int cfd, Client& client, std::string& pass) {
+    auto it = account.find(client.username);
+    if (it == account.end() || it->second != pass) {
+        std::lock_guard<std::mutex> lock(clients[cfd].mutex);
+        clients[cfd].writebuf += "530 password errno\r\n";
+        mod_epoll(cfd, EPOLLIN | EPOLLOUT | EPOLLET);
+        return;
+    } else {
+        clients[cfd].writebuf += "230 login succeful\r\n";
+        mod_epoll(cfd, EPOLLIN | EPOLLOUT | EPOLLET);
+    }
+    client.islogin = true;
+    std::lock_guard<std::mutex> lock(clients[cfd].mutex);
+}
+
+void ftpepollserver::PASV(int cfd, Client& client) {
+    closePASV(client);
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    int opt = 1;
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    IPV4 addr(0);
+    bind(fd, addr.change(), addr.len());
+    listen(fd, 1);
+    socklen_t len = addr.len();
+    getsockname(fd, addr.change(), &len);
+    sockaddr_in caddr{};
+    len = sizeof(caddr);
+    getsockname(cfd, reinterpret_cast<sockaddr*>(&caddr), &len);
+    unsigned char* ip =
+        reinterpret_cast<unsigned char*>(&caddr.sin_addr.s_addr);
+    uint16_t port = addr.getport();
+    int p1 = port / 256;
+    int p2 = port % 256;
+
+    client.pasvfd = fd;
+    std::lock_guard<std::mutex> lock(clients[cfd].mutex);
+    clients[cfd].writebuf +=
+        "227 Entering Passive Mode (" + std::to_string(ip[0]) + "," +
+        std::to_string(ip[1]) + "," + std::to_string(ip[2]) + "," +
+        std::to_string(ip[3]) + "," + std::to_string(p1) + "," +
+        std::to_string(p2) + ")\r\n";
+    mod_epoll(cfd, EPOLLIN | EPOLLOUT | EPOLLET);
+}
+
+int ftpepollserver::acceptdata(Client& client) {
+    int datafd = accept(client.pasvfd, nullptr, nullptr);
+    closePASV(client);
+    return datafd;
+}
+
+void ftpepollserver::LIST(int cfd, Client& client, std::string arg) {
+    int datafd = acceptdata(client);
+    if (arg.empty()) {
+        arg = ".";
+    }
+    DIR* dir = opendir(arg.c_str());
+    std::string datafdwritebuf;
+    if (dir == nullptr) {
+        close(datafd);
+        clients[cfd].writebuf += "550 open dir failed\r\n";
+        mod_epoll(cfd, EPOLLIN | EPOLLOUT | EPOLLET);
+        return;
+    } else {
+        clients[cfd].writebuf += "222 open dir\r\n";
+        mod_epoll(cfd, EPOLLIN | EPOLLOUT | EPOLLET);
+    }
+
+    dirent* ent = nullptr;
+    while ((ent = readdir(dir)) != nullptr) {
+        std::lock_guard<std::mutex> lock(client.mutex);
+        std::string line = std::string(ent->d_name) + "\r\n";
+        if (!sendn(datafd, line.data(), line.size())) {
+            break;
+        }
+    }
+    closedir(dir);
+    close(datafd);
+    clients[cfd].writebuf += "226 list complete\r\n";
+    mod_epoll(cfd, EPOLLIN | EPOLLOUT | EPOLLET);
+}
+void ftpepollserver::RETR(int cfd,
+                          Client& client,
+                          std::string path1,
+                          std::string path2) {
+    int fd = open(path1.c_str(), O_RDONLY);
+    if (fd == -1) {
+        clients[cfd].writebuf += "550 file not found\r\n";
+        mod_epoll(cfd, EPOLLIN | EPOLLOUT | EPOLLET);
+        return;
+    } else {
+        clients[cfd].writebuf += "222 file found\r\n";
+        mod_epoll(cfd, EPOLLIN | EPOLLOUT | EPOLLET);
+    }
+    int datafd = acceptdata(client);
+    char buf[4096];
+    ssize_t n = 0;
+    bool ok = true;
+    while ((n = read(fd, buf, sizeof(buf))) > 0) {
+        if (send(datafd, buf, n, 0) != n) {
+            ok = false;
+            break;
+        };
+    }
+
+    close(fd);
+    close(datafd);
+    if (ok) {
+        clients[cfd].writebuf += "226 retr complete\r\n";
+    } else {
+        clients[cfd].writebuf += "426 connect close\r\n";
+    }
+    mod_epoll(cfd, EPOLLIN | EPOLLOUT | EPOLLET);
+}
+
+void ftpepollserver::STOR(int cfd,
+                          Client& client,
+                          std::string path1,
+                          std::string path2) {
+    int fd = open(path2.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd == -1) {
+        clients[cfd].writebuf += "550 open failed\r\n";
+        mod_epoll(cfd, EPOLLIN | EPOLLOUT | EPOLLET);
+
+    } else {
+        clients[cfd].writebuf += "222 open\r\n";
+        mod_epoll(cfd, EPOLLIN | EPOLLOUT | EPOLLET);
+    }
+    int datafd = acceptdata(client);
+    char buf[4096];
+    ssize_t n = 0;
+    bool ok = true;
+    while ((n = recv(datafd, buf, sizeof(buf), 0)) > 0) {
+        if (write(fd, buf, static_cast<ssize_t>(n)) != n) {
+            ok = false;
+            break;
+        }
+    }
+    close(fd);
+    close(datafd);
+    if (ok) {
+        clients[cfd].writebuf += "226 stor complete\r\n";
+        mod_epoll(cfd, EPOLLIN | EPOLLOUT | EPOLLET);
+    } else {
+        clients[cfd].writebuf += "426 connect close\r\n";
+        mod_epoll(cfd, EPOLLIN | EPOLLOUT | EPOLLET);
+    }
+}
+
+void ftpepollserver::closePASV(Client& client) {
+    if (client.pasvfd != -1) {
+        close(client.pasvfd);
+        client.pasvfd = -1;
+    }
+}
+std::string getlineok(std::string line) {
+    while (!line.empty() && (line.back() == '\n' || line.back() == '\r')) {
+        line.pop_back();
+    }
+    return line;
+}
+bool sendn(int fd, char* data, ssize_t len) {
+    while (len > 0) {
+        ssize_t n = send(fd, data, len, 0);
+        if (n <= 0) {
+            return false;
+        }
+        data += n;
+        len -= static_cast<ssize_t>(n);
+    }
+    return true;
+}
+void set_nonblock(int fd) {
+    int flags = fcntl(fd, F_GETFL, 0);
+    fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+}
+int main() {
+    ftpepollserver server(2100);
+    server.run();
+    return 0;
+}
